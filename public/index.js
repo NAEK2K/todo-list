4045902948abc58a33f6bb48a503e3caee22ce82
NAEK2K <naek2k@outlook.com>
2020-04-30 11:10:46 -0400

    QoL changes, added list functionality

---
+++ public/index.js
@@ -0,0 +1,97 @@
+var tasks;
+var lists;
+var socket = io()
+
+$('#task-input').keyup((e) => {
+    if(e.keyCode === 13) {
+        $('#addTaskBtn').click()
+    }
+})
+
+$('#list-input').keyup((e) => {
+    if(e.keyCode === 13) {
+        $('#addListBtn').click()
+    }
+})
+
+function addTask() {
+    if ($('#task-input').val() == '') return
+    socket.emit('addTask', { task: $('#task-input').val() })
+    $('#task-input').val('')
+    updateTasks()
+}
+
+function addList() {
+    if ($('#list-input').val() == '') return
+    socket.emit('addList', { list: $('#list-input').val() })
+    $('#list-input').val('')
+    updateTasks()
+}
+
+function removeList() {
+    if ($('#list-input').val() == '') return
+    socket.emit('removeList', { list: $('#list-input').val() })
+    $('#list-input').val('')
+    updateTasks()
+}
+
+function updateTasks() {
+    socket.emit('updateTasks')
+    updateLists()
+}
+
+function updateLists() {
+    socket.emit('updateLists')
+}
+
+function tick(x) {
+    socket.emit('tick', { id: x.value })
+    updateTasks()
+}
+
+function renderTasks() {
+    console.log(tasks)
+    $('#tasks > ul').empty()
+    var completed_tasks = []
+    tasks.forEach(task => {
+        var newTask = $('<li class=\"task\"></li>').text(task.task)
+        if (task.completed == true) {
+            newTask.prepend($(`<input type=\"checkbox\" value=\"${task.id}\" onclick=\"tick(this);\" checked><spacer>`))
+            completed_tasks.push(newTask)
+        } else if (task.completed == false) {
+            newTask.prepend($(`<input type=\"checkbox\" value=\"${task.id}\" onclick=\"tick(this);\"><spacer>`))
+            $('#tasks > ul').append(newTask)
+        }
+    })
+    completed_tasks.forEach(task => {
+        $('#tasks > ul').append(task)
+    })
+}
+
+function changeList(x) {
+    socket.emit('changeList', { list: x.getAttribute("value")})
+    updateTasks()
+}
+
+function renderLists() {
+    $('#lists').empty()
+    lists.forEach(list => {
+        var newList = $(`<a class=\"dropdown-item\" onclick=\"changeList(this);\" value=\"${list}\"></a>`).text(list)
+        $('#lists').append(newList)
+    })
+}
+
+function clearChecked() {
+    socket.emit('clearChecked')
+    updateTasks()
+}
+
+socket.on('updateTasks', (data) => {
+    tasks = data.tasks
+    renderTasks()
+})
+
+socket.on('updateLists', (data) => {
+    lists = data.lists
+    renderLists()
+})
--- server.js
+++ server.js
@@ -15,29 +15,50 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // lowdb init
-db.defaults({tasks: []}).write()
+db.defaults({tasks: [], lists: ["Main"]}).write()
 
 // uuidv4
 const { uuid } = require('uuidv4')
 
 io.on('connection', socket => {
+    socket.list = "Main"
     socket.on('addTask', (data) => {
-        db.get('tasks').push({id: uuid(), task: data.task, completed: false}).write()
+        db.get('tasks').push({id: uuid(), task: data.task, completed: false, list: socket.list}).write()
     })
     socket.on('updateTasks', () => {
-        socket.emit('updateTasks', {tasks: db.get('tasks')})
+        socket.emit('updateTasks', {tasks: db.get('tasks').filter((task) => task.list == socket.list).value()})
+    })
+    socket.on('updateLists', () => {
+        socket.emit('updateLists', {lists: db.get('lists').value()})
     })
     socket.on('tick', (data) => {
-        db.get('tasks').find({ id: data.id}).assign({completed: !(db.get('tasks').find({ id: data.id}).get('completed').value())}).value()
-        db.write()
+        db.get('tasks').find({ id: data.id}).assign({completed: !(db.get('tasks').find({ id: data.id}).get('completed').value())}).write()
     })
     socket.on('clearChecked', () => {
         db.get('tasks').remove({ completed: true }).write()
+    })
+    socket.on('changeList', (data) => {
+        socket.list = data.list
+    })
+    socket.on('addList', (data) => {
+        if(db.get('lists').value().includes(data.list)) return
+        db.get('lists').push(data.list).write()
+        socket.list = data.list
+    })
+    socket.on('removeList', (data) => {
+        if(data.list == 'Main') return
+        db.get('tasks').remove({list: data.list}).write()
+        db.get('lists').pull(data.list).write()
+        socket.list = 'Main'
     })
 })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.js'))
 })
 
 server.listen(port, () => {
